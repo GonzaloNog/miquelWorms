d0bcfc1c4a338501feb276f2e5ba03cb4252d5a5
m.muntaner.e@gmail.com <m.muntaner.e@gmail.com>
2023-12-11 14:01:04 +0100

    animaation worms

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D bulletPrefab;
+
+    [SerializeField]
+    private Transform currentGun;
+
+    public float walkSpeed = 1.0f;
+    public float maxRelativeVelocity = 6.0f;
+    public float missileForce = 5.0f;
+
+    public int wormID;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Camera mainCam;
+
+    //public bool isTurn{}
+    //WormHealth wormHealth;
 
     void Start()
     {
